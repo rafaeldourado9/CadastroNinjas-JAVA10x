bd84b3d350bbcbd0d93ab7d1dee4f625e9bb8aa6
Rafael <rafaeldouradoc7@gmail.com>
2026-04-26 14:58:30 -0300

    chore: initialize project

---
+++ src/test/java/cadastroninja/cadastroninja/CadastroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package cadastroninja.cadastroninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
